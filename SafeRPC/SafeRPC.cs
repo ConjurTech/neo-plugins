a6f4bf0ae06d9419af8872c1d8c4fb3c39bf067b
switcheo <henry.chua@switcheo.network>
2018-08-30 17:11:39 +0800

    Implement safe-rpc plugin

---
+++ SafeRPC/SafeRPC.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Neo.IO.Data.LevelDB;
+using Neo.IO.Json;
+using Neo.Ledger;
+using Neo.Network.P2P.Payloads;
+using Neo.Network.RPC;
+using Neo.SmartContract;
+using System.Linq;
+
+namespace Neo.Plugins
+{
+    public class SafeRPC : Plugin, IRpcPlugin
+    {
+        public SafeRPC()
+        {
+
+        }
+
+        public JObject OnProcess(HttpContext context, string method, JArray _params)
+        {
+            if (method == "getrawmempool")
+            {
+                if (_params[0].AsString() == Settings.Default.ApiKey) return null;
+                throw new RpcException(-400, "Access denied");
+            }
+
+            if (method == "sendrawtransaction")
+            {
+                Transaction tx = Transaction.DeserializeFrom(_params[0].AsString().HexToBytes());
+                if (tx.Witnesses.Any(witness => Settings.Default.AllowedWitnesses.Contains(witness.ScriptHash))) return null;
+                throw new RpcException(-400, "Access denied");
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SafeRPC/Settings.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Neo.Wallets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Neo.Plugins
+{
+    internal class Settings
+    {
+        public static Settings Default { get; }
+        public HashSet<UInt160> AllowedWitnesses { get; }
+        public String ApiKey { get; }
+
+        static Settings()
+        {
+            Default = new Settings(Assembly.GetExecutingAssembly().GetConfiguration());
+        }
+
+        public Settings(IConfigurationSection section)
+        {
+            this.AllowedWitnesses = new HashSet<UInt160>(section.GetSection("AllowedWitnesses").GetChildren().Select(p => p.Value.ToScriptHash()));
+            this.ApiKey = section.GetSection("ApiKey").Value.ToString();
+        }
+    }
+}
