599e23a02db784c48bf2740180b6f9a8b8c45664
switcheo <henry.chua@switcheo.network>
2018-10-23 15:29:37 +0800

    Write events into psql

---
+++ EventsWriter/Settings.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Neo.Wallets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Neo.Plugins
+{
+    internal class Settings
+    {
+        public string DatabaseConnString { get; }
+        public HashSet<string> ContractHashList { get; }
+
+        public static Settings Default { get; }
+
+        static Settings()
+        {
+            Default = new Settings(Assembly.GetExecutingAssembly().GetConfiguration());
+        }
+
+        public Settings(IConfigurationSection section)
+        {
+            DatabaseConnString = section.GetSection("DatabaseConnString").Value;
+            ContractHashList = new HashSet<string>(section.GetSection("ContractHashList").GetChildren().Select(p => p.Value.ToString()));
+        }
+    }
+}
